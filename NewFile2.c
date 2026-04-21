c08a80d357a7d2fcb5ef93f8757ac4f353baf9c9
Kayo Veloso <kaioplay260@gmail.com>
2026-04-21 16:01:16 -0300

    Add files via upload

---
+++ NewFile2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+  int main(){
+    char letra;
+    
+    printf("digite uma letra: ");
+    scanf(" %c" ,&letra); // 
+    
+    if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+        letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' ||
+        
+        
+       printf("É uma vogal\n");
+  } else {
+       printf("É uma consoante\n");
+  }
+
+
+
+
+return 0;
+  }
+
+
+
+
